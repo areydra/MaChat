9b592ef67ff819cd60c651f6e47d7398a55036ed
Areydra Desfikriandre <areydra@gmail.com>
2020-07-05 19:46:15 +0700

    move app to src/index

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+      <SafeAreaView>
+          <Text style={{ textAlign: 'center' }}>its will be place for all navigations</Text>
+      </SafeAreaView>
+  );
+};
+
+export default App;
